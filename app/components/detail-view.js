421fe20d1edf9fbf5d8fe7818f568ca60a639102
Sarah Clatterbuck <sclatter@linkedin.com>
2015-05-20 18:04:59 -0700

    minor updates to dialog view styling and closing

--- app/components/detail-view.js
+++ app/components/detail-view.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    close: function() {
+      console.log('closed!');
+    },
+  }
   /*becomeFocused: function() {
   	var section = this.$('#detail-view');
     section.attr('tabindex', -1).focus();
--- app/routes/engineering.js
+++ app/routes/engineering.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
       if (section) {
         section.attr('tabindex', -1).focus();
       }
-    }, 
+    },
     didTransition: function() {
       console.log('trasitioned!');
       return true;
